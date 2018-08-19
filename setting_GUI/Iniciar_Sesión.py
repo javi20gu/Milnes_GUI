6ffd8250691d029bc1ae403b830d13f06d7c6c3c
Javier <javierhidalgo_c@hotmail.com>
2018-08-19 16:54:30 +0200

    Mejoras de Código

--- setting_GUI/Iniciar_Sesión.py
+++ setting_GUI/Iniciar_Sesión.py
@@ -7,7 +7,7 @@ def login(email: str, password: str):
     usuarios = Database.usuarios()
 
     usuario = list(filter(lambda datos: datos[0] == email and datos[1] == password, usuarios))
-    if usuario != []:
+    if usuario:
         return [True, Database.buscar_por_email(email)]
     else: 
         return [False]
